aa87cf03a94942bd94afd963df2bff7f49477ffa
HK <n1dtec@users.noreply.github.com>
2019-11-21 15:06:55 -0800

    Optimzing imports and adding gitignore

--- src/CurrencyExchangeAPI.java
+++ src/CurrencyExchangeAPI.java
@@ -1,23 +1,10 @@
-import kong.unirest.json.JSONObject;
-
 import javax.json.JsonObject;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.text.DecimalFormat;
 
 @Path("/")
 public class CurrencyExchangeAPI {
@@ -36,12 +23,12 @@ public class CurrencyExchangeAPI {
 
     @Path("/currencyCode")
     @GET
-    public JSONObject getCurrencyCode(){
+    public JsonObject getCurrencyCode(){
         Client client = ClientBuilder.newClient();
         WebTarget resource = client.target("http://api.ipgeolocationapi.com/geolocate/" + getIP());
-        JSONObject response = resource.request(MediaType.APPLICATION_JSON)
+        JsonObject response = resource.request(MediaType.APPLICATION_JSON)
                 //.header("IP Address", )
-                .get(JSONObject.class);
+                .get(JsonObject.class);
 
         return response;
     }
