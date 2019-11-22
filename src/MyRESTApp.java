393c494406394f289c838074175d0f97a9c6d61e
HK <n1dtec@users.noreply.github.com>
2019-11-21 18:05:47 -0800

    minor change

--- src/MyRESTApp.java
+++ src/MyRESTApp.java
@@ -6,8 +6,9 @@ import java.util.Set;
 //Defines the base URI for all resource URIs.
 @ApplicationPath("/")
 //The java class declares root resource and provider classes
-public class MyRESTApp extends Application {//The method returns a non-empty collection with classes, that must be included in the published JAX-RS application
+public class MyRESTApp extends Application {
 
+    //The method returns a non-empty collection with classes, that must be included in the published JAX-RS application
     @Override
     public Set<Class<?>> getClasses() {
         HashSet h = new HashSet<Class<?>>();
