bc38676d875b4304d8786b3a645ed7b9d8bca5ad
HK <n1dtec@users.noreply.github.com>
2019-11-21 19:14:56 -0800

    Error Handling and other changes

--- src/CurrencyDescriptionAPI.java
+++ src/CurrencyDescriptionAPI.java
@@ -7,77 +7,166 @@
 import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/")
 public class CurrencyDescriptionAPI {
 
     //Displays welcome text on the default page
     @GET
-    public String welcome(){
+    public String welcome() {
         return "Welcome to the 'Currency Description by Location' Project which is a simple RESTful API project developed by Harnidh Kaur";
     }
 
     //Returns the user's current IP address
     @Path("/ip")
     @GET
-    public String getIP(){
+    public Response getIP() {
 
-        //Call the public API to get the user's IP address
-        Client client = ClientBuilder.newClient();
-        WebTarget resource = client.target("http://api.ipify.org");
-        String response = resource.request(MediaType.APPLICATION_JSON)
-                //.header("IP Address", )
-                .get(String.class);
+        String IP = getIPString();
 
-        return response;
+        if (IP == null) {
+            //Return 500 if there is any error in the execution
+            Response.ResponseBuilder responseBuilder = Response.status(Response.Status.INTERNAL_SERVER_ERROR);
+            return responseBuilder
+                    .header("status", "failure")
+                    .entity("There was an error in execution of the API. Please try after some time.").build();
+        }
+
+        //Return successful response if call is working fine
+        Response.ResponseBuilder responseBuilder = Response.status(Response.Status.OK);
+        return responseBuilder
+                .header("status", "success")
+                .entity(IP).build();
 
     }
 
-    //Returns the information of the country from where the user is calling
+    //Takes an IP as Path Param and returns the information of the country from input IP
+    @Path("/countryInfo/{ip}")
+    @GET
+    public Response getCountryInfo(@PathParam("ip") String IP) {
+
+        JsonObject countryInfo = getCountryInfoObject(IP);
+
+        if (countryInfo == null) {
+
+            //Return 400 as error could be due to incorrect input IP
+            Response.ResponseBuilder responseBuilder = Response.status(Response.Status.BAD_REQUEST);
+            return responseBuilder
+                    .header("status", "failure")
+                    .entity("There was an error in execution of the API. Please check input and try again.").build();
+
+        }
+
+        //Return successful response if call is working fine
+        Response.ResponseBuilder responseBuilder = Response.status(Response.Status.OK);
+        return responseBuilder
+                .header("status", "success")
+                .entity(countryInfo).build();
+
+    }
+
+    //This method is called if the user does not input any IP in the Path Param
     @Path("/countryInfo")
     @GET
-    public JsonObject getCountryInfo(){
+    public Response displayErrorMessage() {
 
-        //Call the public API to get geolocation information
-        Client client = ClientBuilder.newClient();
-        WebTarget resource = client.target("http://api.ipgeolocationapi.com/geolocate/" + getIP());
-        JsonObject response = resource.request(MediaType.APPLICATION_JSON)
-                .get(JsonObject.class);
-
-        return response;
+        //Return 400 as we are expecting an input IP in path params
+        Response.ResponseBuilder responseBuilder = Response.status(Response.Status.BAD_REQUEST);
+        return responseBuilder
+                .header("status", "failure")
+                .entity("Please enter a valid IP address in the path and try again.").build();
 
     }
 
     //Returns the description of the currency of the country from where the user is calling
     @Path("/currencyInfo")
     @GET
-    public String getCurrencyInfo(){
+    public Response getCurrencyInfo() {
 
-        JsonObject countryInfo = getCountryInfo();
+        try {
 
-        String countryName = countryInfo.getString("name");
-        String currencyCode = countryInfo.getString("currency_code");
+            //Get IP and country information
+            String IP = getIPString();
+            JsonObject countryInfo = getCountryInfoObject(IP);
 
-        //Call the public API to get information about currency
-        Client client = ClientBuilder.newClient();
-        WebTarget resource = client.target("http://v2.api.forex/infos/currency/" + currencyCode + ".json");
-        JsonObject response = resource.request(MediaType.APPLICATION_JSON).get(JsonObject.class);
+            String countryName = countryInfo.getString("name");
+            String currencyCode = countryInfo.getString("currency_code");
 
-        JsonObject currencyInfo = response.getJsonObject(currencyCode);
-        String currencyName = currencyInfo.getString("name");
-        String currencySymbol = currencyInfo.getString("symbol");
+            //Call the public API to get information about currency
+            Client client = ClientBuilder.newClient();
+            WebTarget resource = client.target("http://v2.api.forex/infos/currency/" + currencyCode + ".json");
+            JsonObject response = resource.request(MediaType.APPLICATION_JSON).get(JsonObject.class);
 
-        //Build the description
-        String description = "You are living in " + countryName + " where "
-                + " the currency is " + currencyName + ".\n"
-                + "The currency symbol is " + currencySymbol + " and the currency code is " + currencyCode + ".\n";
+            JsonObject currencyInfo = response.getJsonObject(currencyCode);
+            String currencyName = currencyInfo.getString("name");
+            String currencySymbol = currencyInfo.getString("symbol");
 
-        return description;
+            //Build the description
+            String description = "You are living in " + countryName + ", where "
+                    + " the currency is " + currencyName + ".\n"
+                    + "The currency symbol is " + currencySymbol + " and the currency code is " + currencyCode + ".\n";
 
+            //Return successful response if everything works fine
+            Response.ResponseBuilder responseBuilder = Response.status(Response.Status.OK);
+            return responseBuilder
+                    .header("status", "success")
+                    .entity(description).build();
+
+        } catch (Exception e) {
+
+            //Return 500 if there is any error in the execution
+            Response.ResponseBuilder responseBuilder = Response.status(Response.Status.INTERNAL_SERVER_ERROR);
+            return responseBuilder
+                    .header("status", "failure")
+                    .entity("There was an error in execution of the API. Please try after some time.").build();
+
+        }
+
+    }
+
+
+    private String getIPString() {
+        try {
+
+            //Call the public API to get the user's IP address
+            Client client = ClientBuilder.newClient();
+            WebTarget resource = client.target("http://api.ipify.org");
+            String response = resource.request(MediaType.APPLICATION_JSON)
+                    .get(String.class);
+            return response;
+
+        } catch (Exception e) {
+
+            //Returns null if there is any error in execution of the request
+            return null;
+
+        }
+    }
+
+    private JsonObject getCountryInfoObject(String IP) {
+
+        try {
+
+            //Call the public API to get geolocation information
+            Client client = ClientBuilder.newClient();
+            WebTarget resource = client.target("http://api.ipgeolocationapi.com/geolocate/" + IP);
+            JsonObject response = resource.request(MediaType.APPLICATION_JSON)
+                    .get(JsonObject.class);
+
+            return response;
+
+        } catch (Exception e) {
+
+            //Returns null if there is any error in execution of the request
+            return null;
+
+        }
     }
 
 }
